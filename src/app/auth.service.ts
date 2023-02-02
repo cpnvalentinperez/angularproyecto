afe80f205ba4edf0b04cb037c121792a09cf6e9c
vperezarrascaeta <vperezarrascaeta@epe.santafe.gov.ar>
2023-02-02 09:12:27 -0300

    algunos cambios

---
+++ src/app/auth.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClientModule,HttpClient} from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthService {
+  
+  api = 'https://localhost:3000/api';
+  token; 
+  
+  constructor(private http: HttpClient,private router: Router) { }
+  
+  login(email: string, password: string) {
+    this.http.post(this.api + '/authenticate', {email: email, password: password})
+    .subscribe((resp: any) => {
+    //Redireccionamos al ususario a su perfil
+        this.router.navigate(['perfil']);
+        //Guardamos el token en localStorage
+        localStorage.setItem('auth_token', resp.token);
+      })
+    );
+  }
+
+  //Para cerrar sesion eliminamos el token del localStorage
+  logout() {
+    localStorage.removeItem('token');
+  }
+
+  // Un servicio para verificar si existe la sesion
+  public get logIn(): boolean {
+    return (localStorage.getItem('token') !== null);
+  }
+}
+  
