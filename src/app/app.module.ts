fc8fd57eaa136dccf7ede51432093d6f2932bf39
cpnvalentinperez <cpnvalentinperez@gmail.com>
2023-02-26 18:18:31 -0300

    Anda el login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [BrowserModule,FormsModule,AppRoutingModule],
-  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  declarations: [AppComponent, LoginComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
 import { Router, ActivatedRoute} from '@angular/router';
 import { AuthService } from '../auth.service';
+
+
 
 @Component({
   selector: 'app-login',
@@ -11,7 +13,23 @@ export class LoginComponent implements OnInit {
 
   constructor() { }
 
+  
   ngOnInit() {
+  }
+
+  nombre: string = '';
+  clave: string = '';
+  // telefono = '';
+  // dni = '';
+
+  // cambiaDni(valor:any) {
+  //   // Podes consultar el valor por consola
+  //   console.log(valor);
+  //   this.dni = valor;
+  // }
+
+  onClick() {
+    alert('Ahora se tu clave!!');
   }
 
 }
